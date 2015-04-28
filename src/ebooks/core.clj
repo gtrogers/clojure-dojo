a7e01a87dffaf56d4fd52352d021ea67ae97ad87
Gareth <grogers@thoughtworks.com>
2015-04-28 19:41:59 +0100

    Initial commit!

---
+++ src/ebooks/core.clj
@@ -0,0 +1,6 @@
+(ns ebooks.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/ebooks/core_test.clj
@@ -0,0 +1,7 @@
+(ns ebooks.core-test
+  (:require [clojure.test :refer :all]
+            [ebooks.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
